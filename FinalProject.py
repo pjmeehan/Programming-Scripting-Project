84114b05dc40813c6d8d5aa14345bd971279dff5
Patrick Meehan <G00364732@gmit.ie>
2018-04-14 12:07:48 +0100

    initial script

---
+++ FinalProject.py
@@ -0,0 +1,17 @@
+# April 14, 2008 Patrick Meehan
+# Programming and Scripting Final Project
+# reference https://shapeofdata.wordpress.com/2013/10/01/case-study-1-iris/
+
+
+# labelnames translates the species names into integers
+labelnames = {"Iris-setosa\n":0, 
+              "Iris-versicolor\n":1, 
+              "Iris-virginica\n":2}
+data, labels = [], []      # Empty lists for the vectors, labels.
+f = open("iris.data", "r") # Open the data file.
+for line in f:             # Load in each line from the file. 
+    vals = line.split(',') # Split the line into individual values
+    if len(vals) == 5:     # Check that there are five columns
+        data.append(vals[:-1])              # Add data vector
+        labels.append(labelnames[vals[4]])  # Add numerical label
+f.close()                  # Close the file
