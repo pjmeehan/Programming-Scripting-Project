bad306bf29468d313d79e611cd818abc95fdc96f
Patrick Meehan <G00364732@gmit.ie>
2018-04-19 16:35:37 +0100

    updated

--- FinalProject.py
+++ FinalProject.py
@@ -1,44 +1,72 @@
 # April 14, 2008 Patrick Meehan
 # Programming and Scripting Final Project
-# reference https://shapeofdata.wordpress.com/2013/10/01/case-study-1-iris/
-# try to incorporate some of the script referenced above into my initial script.
 
-# import data set using previous script to do so
 
-# import libraries
+# notes
+# # import data set using previous script to do so
+# The following references were used to help with the project
+# 1. https://diwashrestha.com/2017/09/18/machine-learning-on-iris/
+# 2. http://www.pyzo.org/python_vs_matlab.html
+# 3. https://en.wikipedia.org/wiki/Pandas_(software)
+# 4. https://en.wikipedia.org/wiki/Iris_flower_data_set#Data_set
+# 5. http://www.pythonforbeginners.com/systems-programming/using-the-csv-module-in-python/
+# 6. https://dzone.com/articles/python-101-reading-and-writing
+# 7. https://www.datacamp.com/community/tutorials/pandas-tutorial-dataframe-python
+# 8. https://campus.datacamp.com/courses/pandas-foundations/exploratory-data-analysis?ex=5#skiponboarding
 
-import numpy as np
+
+# import Matlab and Pandas libraries
+
+
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
+# Matlab is a commercial numerical computing environment and programming language. The concept of Matlab refers to the whole package, 
+#   including the IDE. The standard library does not contain as much generic programming functionality, but does include matrix algebra 
+#   and an extensive library for data processing and plotting.[2]
+# Pandas is a popular Python package for data science, and with good reason: it offers powerful, expressive and flexible data structures 
+#   that make data manipulation and analysis easy, among many other things. The DataFrame is one of these structures.[3]
 
-# use the import function to import the iris data set.
 
 import csv 
+# CSV module is a built-in function that allows Python to parse these csv types of files.[5]
+# use the import function to import the iris data set.
+# to see the full dataset, please go to the following website : https://en.wikipedia.org/wiki/Iris_flower_data_set#Data_set [4]
 
-# use open function to open the file with the data.
 
 x = open("data/iris.csv")  
+# use open function to open the file with the data.
 
-# The reader object allows iteration, much like a regular file object does. This let’s us iterate over each row 
-# in the reader object and print out the line of data, minus the commas. This works because each row is a list
-# and we can join each element in the list together, forming one long string.
-# https://dzone.com/articles/python-101-reading-and-writing
 
 csv_x = csv.reader(x)
+# The reader object allows iteration, much like a regular file object does. This allows for iteration over each row 
+# in the reader object and the print out of the line of data, minus the commas. This works because each row is a list
+# and we can join each element in the list together, forming one long string.[6]
 
-# use a for statement to run through each row.
-
-
-# use print statement along with format to format the data in each row with certain amount of spaces. 
-  
-  
+ 
 iris = pd.read_csv("data/iris.csv")
-print (iris)
+# reads the dataset into the DataFrame. 
+# DataFrame is an expressive and flexible data structure that makes data manipulation and analysis easy.[7]
+# reference [1]
+
+print (iris.head(10))
+# the .head() function allows the printing of certain rows. .head() is the default for the first 5 rows. I chose to run the first 10 rows in my script.
+
+
 print (iris.shape)
+# the .shape function will give the dimensions of the array. In the iris dataset it is 150 rows x 5 columns. 
+# reference [1]
+ 
 print(iris.describe())
+# the .describe will give an important statistical summary of each column in the dataset.
+# reference [1]
 
+# plotting
 
-# histograms
 iris.hist()
+# .hist() method generates histograms andut also plots the probability density functions (PDFs) and cumulative density functions (CDFs).
+# reference [8] 
+# reference [1]
+
 plt.show()
+# .show() allows for the image,(a graph in this case) to be shown.
+# reference [1]
